85eb1d3087afb8c49cc240c742313f65047d7458
Collabos_Ikeda <r-ikeda@collabos.com>
2024-11-16 13:30:55 +0900

    fix

---
+++ src/components/HeaderComponents/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+    <header>
+        <div className='logo'>
+            <h3>会社名</h3>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                  <a href='#'>aa</a>
+                </li>
+                <li>
+                  <a href='#'>bb</a>
+                </li>
+                <li>
+                  <a href='#'>cc</a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+
+  )
+}
+
+export default Header
