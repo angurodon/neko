8a9f1f4ffa7ef23dc391e38f0744ed68d7d0c8c6
Collabos_Ikeda <r-ikeda@collabos.com>
2024-11-16 14:57:02 +0900

    Footer

--- src/components/HeaderComponents/Header.jsx
+++ src/components/HeaderComponents/Header.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Header.css'; // CSSファイルのインポート
+import logo from '../../assets/logo_side.png';
 
 const Header = () => {
   return (
+    <header className='header'>
+      <div className='logo'>
+        <img src={logo} alt="会社ロゴ" />
+      </div>
 
-    <header>
-        <div className='logo'>
-            <h3>会社名</h3>
-        </div>
+      <nav>
+        <ul className='nav-list'>
+          <li><Link to="/">ホーム</Link></li>
+          <li><Link to="/features">特徴</Link></li>
+          <li><Link to="/community">コミュニティ</Link></li>
+        </ul>
+      </nav>
 
-        <nav>
-            <ul>
-                <li>
-                  <a href='#'>aa</a>
-                </li>
-                <li>
-                  <a href='#'>bb</a>
-                </li>
-                <li>
-                  <a href='#'>cc</a>
-                </li>
-
-            </ul>
-        </nav>
+      <div className='contact-btn'>
+        <a href="/contact" className="btn">お問い合わせ</a>
+      </div>
     </header>
-
-  )
+  );
 }
 
-export default Header
+export default Header;
