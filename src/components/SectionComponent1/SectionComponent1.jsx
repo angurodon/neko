8cc08d7343258a9485b5b9d640acb806e3af0cbf
Collabos_Ikeda <r-ikeda@collabos.com>
2024-11-17 12:17:35 +0900

    over-scrollの追加

--- src/components/SectionComponent1/SectionComponent1.jsx
+++ src/components/SectionComponent1/SectionComponent1.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import './SectionComponent1.css'; // コンポーネント専用のCSSをインポート
+import './SectionComponent1.css'; // 修正版CSSをインポート
 import logo from '../../assets/Ic-Growth_color_1.png';
-
 
 const SectionComponent1 = () => {
   return (
     <div className="homepage-container">
       <div className="content-section">
-        <h1>文章</h1>
-        <p>概要</p>
-        <button className="contact-button">お問い合わせ</button>
+
+        <p>
+          個人と企業の成長に貢献します！<br />
+          Contribute to the growth of individuals and companies
+        </p>
+        <p>
+          <strong>ミッション:</strong> 日本の近未来における厳しい環境化であっても、持続的成長を遂げるために、
+          有限である経営資源（ヒト、モノ、カネ、時間）活用の最適化を図ることを使命とします。<br></br>
+          我々はこの使命を「会計をエネルギーに変換すること」で実現します。「Accounting Transformation」「AX」の実現を目指します。
+        </p>
       </div>
       <div className="logo-section">
         <img src={logo} alt="Ic-Growth Logo" />
